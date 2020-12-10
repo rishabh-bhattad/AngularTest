54b5d5a405c59d4cae9c645bf568b0142e644cde
Rishabh Bhattad <rishabh.bhattad@gmail.com>
2020-12-10 13:14:27 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
@@ -12,24 +12,24 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
-  });
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test'`, () => {
+  it(`should have as title 'product-mgmt'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('product-mgmt');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('product-mgmt app is running!');
   });
 });
---
+++ src/app/create-product/create-product.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
+export class CreateProductComponent implements OnInit {
+
+  constructor(private productService: ProductService, private router: Router) { }
+
+  product: Product = new Product();
+  submitted 
+
+  ngOnInit() {
+  }
+
+  saveProduct() {
+    this.productService.createProduct(this.product)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.product = new Product();
+    this.gotoProductList();
+  }
+
+  onSubmit() {
+    this.saveProduct();    
+  }
+
+  gotoProductList() {
+    this.router.navigate(['/products']);
+  }
+
+}
